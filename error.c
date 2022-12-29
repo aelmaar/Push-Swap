4747163e0a9dc07dfef13d9701ba07e550e271a5
Anouar El Maaroufi <anouarelmaaroufi@gmail.com>
2022-12-29 23:04:19 +0100

    handle push swap errors, checking if there are duplicates in the args.

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 17:37:32 by ael-maar          #+#    #+#             */
-/*   Updated: 2022/12/29 18:43:54 by ael-maar         ###   ########.fr       */
+/*   Updated: 2022/12/29 20:26:00 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,32 @@ static void	print_list(t_list *head)
 	}
 }
 
-// static void	print_error_and_clear(t_list *head)
-// {
-// 	ft_lstclear(&head);
-// 	ft_putstr_fd("Error\n", 2);
-// 	exit(1);
-// }
+static void	print_error_and_clear(t_list *head)
+{
+	ft_lstclear(&head);
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
+}
+
+static void	check_duplicates(t_list *head)
+{
+	t_list	*temp;
+
+	if (!head)
+		return ;
+	temp = head;
+	head = head->next;
+	while (temp)
+	{
+		while (head)
+		{
+			if (temp->content == head->content)
+				print_error_and_clear(head);
+			head = head->next;
+		}
+		temp = temp->next;
+	}
+}
 
 static void	check_alpha_args(int argc, char *argv[], t_list **head)
 {
@@ -35,26 +55,24 @@ static void	check_alpha_args(int argc, char *argv[], t_list **head)
 	int		j;
 	t_list	*new;
 
-	if (argc > 1)
+	i = 1;
+	while (argv[i])
 	{
-		i = 1;
-		while (argv[i])
+		j = 0;
+		while (argv[i][j])
 		{
-			j = 0;
-			while (argv[i][j])
+			while (argv[i][j] == ' ')
+				j++;
+			if (argv[i][j])
 			{
-				while (argv[i][j] == ' ')
-					j++;
-				if (argv[i][j])
-				{
-					new = ft_lstnew(ft_atoi(&argv[i][j], *head));
-					ft_lstadd_front(head, new);
-				}
-				while (argv[i][j] && argv[i][j] != ' ')
-					j++;
+				new = ft_lstnew(ft_atoi(&argv[i][j], *head));
+				ft_lstadd_front(head, new);
+				check_duplicates(*head);
 			}
-			i++;
+			while (argv[i][j] && argv[i][j] != ' ')
+				j++;
 		}
+		i++;
 	}
 }
 
