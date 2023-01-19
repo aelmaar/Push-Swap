aa6c6b0aa2085d473725055c84cc8e4e10aedcd9
Anouar El Maaroufi <anouarelmaaroufi@gmail.com>
2023-01-19 15:55:49 +0100

    Instead of calculating every element and choose the best element that requires less operations to push to stack b at the first time, I implement another algorithm by adding another sorted stack and divide it every time to middle and the numbers that are less than the middle I push it to stack b until I have left three elements, I sort those three elements using a simple algorithm, after that I calculate element by element in stack b and choose the best element that requires less operations to push to stack a until the stack b is empty, the files is not organized with the newly implemented algorithm, the instructions before on 500 random numbers is less then 5300, now its less than 4300, which is better, proud :)

--- push_swap_srcs/push_swap.c
+++ push_swap_srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 19:17:50 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/01/18 16:33:16 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/01/19 15:23:26 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,26 +37,140 @@ static void	small_to_top(t_list **stack_a, t_list **stack_b)
 	}
 }
 
+// static void	from_a_to_b(t_list **stack_a, t_list **stack_b)
+// {
+// 	t_list			*positions;
+// 	t_enumeration	max_val;
+// 	t_enumeration	min_val;
+
+// 	positions = NULL;
+// 	while (*stack_a && ft_lstsize(*stack_a) >= 3)
+// 	{
+// 		if (ft_lstsize(*stack_a) == 3)
+// 		{
+// 			three_elements(stack_a, stack_b);
+// 			break ;
+// 		}
+// 		max_val = max(*stack_b);
+// 		min_val = min(*stack_b);
+// 		positions = calc_positions(*stack_a, *stack_b, max_val, min_val);
+// 		perfect_position(positions, stack_a, stack_b);
+// 		ft_lstclear(&positions);
+// 	}
+// }
+
+static void	copy_stack(t_list *stack_a, t_list **cp)
+{
+	while (stack_a)
+	{
+		ft_lstadd_back(cp, ft_lstnew(stack_a->content));
+		stack_a = stack_a->next;
+	}
+}
+
+static void	sort_stack(t_list *stack)
+{
+	t_list	*temp;
+	int		temp_swap;
+
+	while (stack)
+	{
+		temp = stack;
+		while (temp)
+		{
+			if (stack->content > temp->content)
+			{
+				temp_swap = stack->content;
+				stack->content = temp->content;
+				temp->content = temp_swap;
+			}
+			temp = temp->next;
+		}
+		stack = stack->next;
+	}
+}
+
+static void	check_from_top_first(t_list **stack_a, t_list **stack_b, t_list *cp)
+{
+	while (*stack_a && (*stack_a)->content < cp->content)
+	{
+		run_actions_for_push_swap("pb\n", stack_a, stack_b);
+	}
+}
+
+static void	check_from_bottom(t_list **stack_a, t_list **stack_b, t_list *cp)
+{
+	t_list	*temp;
+
+	while (*stack_a)
+	{
+		temp = *stack_a;
+		while (temp->next)
+			temp = temp->next;
+		if (temp->content < cp->content)
+		{
+			run_actions_for_push_swap("rra\n", stack_a, stack_b);
+			run_actions_for_push_swap("pb\n", stack_a, stack_b);
+		}
+		else
+			return ;
+	}
+}
+
+static void	check_from_top_again(t_list **stack_a, t_list **stack_b, t_list *cp)
+{
+	t_list	*temp;
+	int		index_a;
+
+	temp = *stack_a;
+	index_a = 0;
+	while (temp)
+	{
+		if (temp->content < cp->content)
+		{
+			while (index_a > 0)
+			{
+				run_actions_for_push_swap("ra\n", stack_a, stack_b);
+				index_a--;
+			}
+			run_actions_for_push_swap("pb\n", stack_a, stack_b);
+			temp = *stack_a;
+			continue ;
+		}
+		index_a++;
+		temp = temp->next;
+	}
+}
+
+static void	push_to_b(t_list **stack_a, t_list **stack_b, t_list *cp)
+{
+	int	middle;
+	int	i;
+
+	while (ft_lstsize(*stack_a) > 3)
+	{
+		i = 1;
+		middle = ft_lstsize(cp) / 2;
+		while (cp && i < middle)
+		{
+			cp = cp->next;
+			i++;
+		}
+		check_from_top_first(stack_a, stack_b, cp);
+		check_from_bottom(stack_a, stack_b, cp);
+		check_from_top_again(stack_a, stack_b, cp);
+	}
+}
+
 static void	from_a_to_b(t_list **stack_a, t_list **stack_b)
 {
-	t_list			*positions;
-	t_enumeration	max_val;
-	t_enumeration	min_val;
+	t_list	*cp;
 
-	positions = NULL;
-	while (*stack_a && ft_lstsize(*stack_a) >= 3)
-	{
-		if (ft_lstsize(*stack_a) == 3)
-		{
-			three_elements(stack_a, stack_b);
-			break ;
-		}
-		max_val = max(*stack_b);
-		min_val = min(*stack_b);
-		positions = calc_positions(*stack_a, *stack_b, max_val, min_val);
-		perfect_position(positions, stack_a, stack_b);
-		ft_lstclear(&positions);
-	}
+	cp = NULL;
+	copy_stack(*stack_a, &cp);
+	sort_stack(cp);
+	push_to_b(stack_a, stack_b, cp);
+	ft_lstclear(&cp);
 }
 
 static void	from_b_to_a(t_list **stack_a, t_list **stack_b)
@@ -76,21 +190,27 @@ static void	from_b_to_a(t_list **stack_a, t_list **stack_b)
 	}
 }
 
-static void	push_swap_run(t_list **stack_a, t_list **stack_b)
-{
-	from_a_to_b(stack_a, stack_b);
-	from_b_to_a(stack_a, stack_b);
-	small_to_top(stack_a, stack_b);
-}
+// static void	push_swap_run(t_list **stack_a, t_list **stack_b)
+// {
+// 	from_a_to_b(stack_a, stack_b);
+// 	if (ft_lstsize(*stack_a) == 3)
+// 		three_elements(stack_a, stack_b);
+// 	from_b_to_a(stack_a, stack_b);
+// 	small_to_top(stack_a, stack_b);
+// }
 
 void	push_swap(t_list **stack_a, t_list **stack_b)
 {
-	if (ft_lstsize(*stack_a) > 4)
-	{
-		run_actions_for_push_swap("pb\n", stack_a, stack_b);
-		run_actions_for_push_swap("pb\n", stack_a, stack_b);
-	}
-	else if (ft_lstsize(*stack_a) > 3)
-		run_actions_for_push_swap("pb\n", stack_a, stack_b);
-	push_swap_run(stack_a, stack_b);
+	// if (ft_lstsize(*stack_a) > 4)
+	// {
+	// 	run_actions_for_push_swap("pb\n", stack_a, stack_b);
+	// 	run_actions_for_push_swap("pb\n", stack_a, stack_b);
+	// }
+	// else if (ft_lstsize(*stack_a) > 3)
+	// 	run_actions_for_push_swap("pb\n", stack_a, stack_b);
+	from_a_to_b(stack_a, stack_b);
+	if (ft_lstsize(*stack_a) == 3)
+		three_elements(stack_a, stack_b);
+	from_b_to_a(stack_a, stack_b);
+	small_to_top(stack_a, stack_b);
 }
