113ee8fcc63cc159d7530e87cd2394cdead081a1
Anouar El Maaroufi <anouarelmaaroufi@gmail.com>
2023-01-20 20:12:21 +0100

    fix some norm error

--- Includes/actions.h
+++ Includes/actions.h
@@ -6,14 +6,14 @@
 /*   By: ael-maar <ael-maar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 20:51:43 by ael-maar          #+#    #+#             */
-/*   Updated: 2023/01/20 13:13:13 by ael-maar         ###   ########.fr       */
+/*   Updated: 2023/01/20 20:12:05 by ael-maar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ACTIONS_H
 # define ACTIONS_H
 
-#include "libft.h"
+# include "libft.h"
 
 void	rotate(t_list **stack, int repeat);
 void	rev_rotate(t_list **stack, int repeat);
